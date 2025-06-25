ab0d0d06703f0608fa519ca2ace25c42bb96bf11
Darian Prusac <darian.prusac@mailbox.org>
2025-06-25 19:36:22 +0200

    It is now possible to get the calendar for every year

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     public static String[] buildMonth(int indexDayStart, int numberDays){
